3f5522a389cf3302ee3355e05e1b416bf0d42872
lloyd3486 <109326784+lloyd3486@users.noreply.github.com>
2023-02-23 04:53:28 +0800

    Update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,11 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    # Removes duplicate from list 2
+    fave_numbers_2 = list(set(fave_numbers_2))
+
+    # Returns false if not all numbers in list 2 are in list 1 (size of list 2 != number of matches with list 1)
+    if len(fave_numbers_2) != len(set(fave_numbers_1).intersection(fave_numbers_2)):
+        return False
 
     return True
